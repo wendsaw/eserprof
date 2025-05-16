5976c0a21f23f90514ab7e02a60007cc099974dd
wendsaw <sawadogo.edgard@gmail.com>
2025-05-16 07:04:50 -0600

    @latest

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// src/main.jsx
+// Force le HTML en français dès le chargement
+document.documentElement.lang = 'fr';
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
